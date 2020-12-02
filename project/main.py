7c31e72968dfba9c7f690157d4a732f6c9266e78
ArturSkrzeta <65757653+ArturSkrzeta@users.noreply.github.com>
2020-12-02 22:17:12 +0100

    Add files via upload

--- project/main.py
+++ project/main.py
@@ -1,14 +1,26 @@
 import cv2
 import pytesseract
 
-pytesseract.pytesseract.tesseract_cmd = r"C:\Tesseract-OCR\tesseract.exe"
+TESSERACT_DIR = r"C:\Tesseract-OCR\tesseract.exe"
 
-img = cv2.imread('page.jpg')
-img = cv2.resize(img, None, fx=0.6, fy=0.6)
-grayed_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-adaptive_threshold = cv2.adaptiveThreshold(grayed_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, 11)
-text = pytesseract.image_to_string(adaptive_threshold, config="--psm 3")
-print(text)
+def main():
 
-cv2.imshow('image', adaptive_threshold)
-cv2.waitKey(0)
+    pytesseract.pytesseract.tesseract_cmd = TESSERACT_DIR
+
+    # reading image in
+    img = cv2.imread('page.jpg')
+    # resizing image
+    img = cv2.resize(img, None, fx=0.6, fy=0.6)
+    # plaining image
+    grayed_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    adaptive_threshold = cv2.adaptiveThreshold(grayed_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, 11)
+    # convering image to txt
+    text = pytesseract.image_to_string(adaptive_threshold, config="--psm 3")
+    # printing text
+    print(text)
+    # showing plain image
+    # cv2.imshow('image', adaptive_threshold)
+    # cv2.waitKey(0)
+
+if __name__ == '__main__':
+    main()
